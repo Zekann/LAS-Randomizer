d49b4f48342421efc019736ae34c47543193aa89
Zekann <zekann3@gmail.com>
2024-10-15 19:33:01 +0200

    Change various code

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import sys
 from cx_Freeze import setup, Executable
 from RandomizerCore.randomizer_data import VERSION
-from py2app import setup as macsetup
 
-build_exe_options = {"packages": ["os"], 
+
+build_exe_options = {"packages": ["os"],
                     "excludes": ["tkinter", "unittest", "sqlite3", "numpy", "matplotlib", "zstandard"],
                     "zip_include_packages": ["encodings", "PySide6"],
                     "include_files": ["RandomizerCore/Data", "RandomizerUI/Resources", "version.txt"],
@@ -12,8 +12,19 @@ build_exe_options = {"packages": ["os"],
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+    build_icon = "RandomizerUI/Resources/icon.ico"
+
+    setup(
+        name = "Links Awakening Switch Randomizer",
+        version = f"{VERSION}",
+        description = "A randomizer for The Legend of Zelda: Link's Awakening remake!",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("randomizer.py", base=base, target_name="Links Awakening Switch Randomizer", icon=build_icon)]
+    )
 
 elif sys.platform == "darwin":
+    from py2app import setup as macsetup
+    build_icon = "RandomizerUI/Resources/icon.icns"
     APP = ['randomizer.py']
     DATA_FILES = ['RandomizerUI/Resources/icon.icns']
     OPTIONS = {'argv_emulation': True}
@@ -23,15 +34,3 @@ elif sys.platform == "darwin":
         options={'py2app': OPTIONS},
         setup_requires=['py2app']
     )
-
-build_icon = "RandomizerUI/Resources/icon.ico"
-if sys.platform == "darwin": # mac
-    build_icon = "RandomizerUI/Resources/icon.icns"
-
-setup(
-    name = "Links Awakening Switch Randomizer",
-    version = f"{VERSION}",
-    description = "A randomizer for The Legend of Zelda: Link's Awakening remake!",
-    options = {"build_exe": build_exe_options},
-    executables = [Executable("randomizer.py", base=base, target_name="Links Awakening Switch Randomizer", icon=build_icon)]
-)
